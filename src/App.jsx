af044a9189278ff8887214a7958948561e33e046
CarloDettori <dettoric07@gmail.com>
2025-04-16 11:42:22 +0200

    search bar for milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useState, useEffect } from "react"
+import { useState, useEffect, memo } from "react"
 
 
-function PoliticiansCard({ id, name, image, position, biography }) {
-
-
-
-
-
+const PoliticiansCard = memo(({ id, name, image, position, biography }) => {
   return (
-
     <div key={id}>
       <h1>{name}</h1>
       <img src={image} alt="" />
       <h2>{position}</h2>
       <p>{biography}</p>
     </div>
-
-
   )
-}
+})
 
 function App() {
   const url = "https://boolean-spec-frontend.vercel.app/freetestapi/politicians/"
   const [politicians, setPoliticians] = useState([])
+  const [filteredPoliticians, setFilteredPoliticians] = useState([]);
+  const [userInput, setUserInput] = useState('');
 
   async function getdata(url) {
     const data = await fetch(url)
@@ -31,9 +25,27 @@ function App() {
     return usedata
   }
 
+  function onCahnge(event) {
+    const input = event.target.value;
+    setUserInput(input);
+
+    if (input === '') {
+      setFilteredPoliticians(politicians);
+    } else {
+      const filtered = politicians.filter((politician) => {
+        return (
+          politician.name.includes(input) ||
+          politician.biography.includes(input)
+        );
+      });
+      setFilteredPoliticians(filtered);
+    }
+  }
+
   useEffect(() => {
     getdata(url).then((res) => {
       setPoliticians(res);
+      setFilteredPoliticians(res);
       console.log(res)
     })
   }, [])
@@ -41,8 +53,9 @@ function App() {
 
   return (
     <main>
-      <h1>ciao</h1>
-      {politicians.map((politician) => {
+      <h1>POLITICI</h1>
+      <input type="text" onChange={onCahnge} value={userInput} />
+      {filteredPoliticians.map((politician) => {
         return <PoliticiansCard name={politician.name} image={politician.image} position={politician.position} biography={politician.biography} />
       })}
     </main>
